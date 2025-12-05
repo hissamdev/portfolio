7a60a228e70b5e8bc8b15daf33a42ec9f2678d84
Hissam <hissamdev@gmail.com>
2025-12-06 01:07:06 +0600

    Deleted extra Layout file in /portfolio

--- app/(root)/portfolio/layout.tsx
+++
@@ -1,8 +0,0 @@
-export default function ProjectsLayout({ children, slot }: {children: React.ReactNode, slot: React.ReactNode;}) {
-  return (
-      <>
-        {children}
-        {slot}
-      </>
-  );
-}
