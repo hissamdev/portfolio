a6d892882d577a18b9f5972c2d668abf46fd2297
Hissam <hissamemail@gmail.com>
2026-01-30 13:35:10 +0600

    Prisma has been set up with a basic schema

--- app/components/home-ui/Hero.tsx
+++ app/components/home-ui/Hero.tsx
@@ -10,11 +10,11 @@ export default function Hero() {
                             <div className="w-7 h-px bg-indigo-500/50 "></div>
                             <span className="text-[#818cf8] text-[12.25px] font-medium tracking-wide ">Hey, I'm Ahmed Hissam</span>
                         </div>
-                        <h1 className="pt-[18px] text-white text-[31px] leading-9 font-bold">Full Stack Services <br />
-                            <span className="bg-[linear-gradient(90deg,#60A5FA,#A78BFA,#F472B6)] bg-clip-text text-transparent">Website, SEO, SMM</span>
+                        <h1 className="pt-[18px] text-white text-[31px] leading-9 font-bold">Full Stack Engineer <br />
+                            <span className="bg-[linear-gradient(90deg,#60A5FA,#A78BFA,#F472B6)] bg-clip-text text-transparent">Custom websites</span>
                         </h1>
                         <p className="pt-3.5 text-[15.75px] leading-relaxed font-medium text-[#94a3b8]">
-                            Building highly performant websites ranging from basic websites to complex ecommerce and SaaS platforms for small to medium sized businesses.</p>
+                            Building highly performant websites that are custom made and hand coded aiming to be rich in features and functionality.</p>
                     </div>
 
                     <div className="">
@@ -24,21 +24,21 @@ export default function Hero() {
                                     <div className=" pr-px flex w-4 h-4 justify-center items-center rounded-full bg-white/3 border border-white/10">
                                         <Check color="#475569" size={12} strokeWidth={3} />
                                     </div>
-                                    <p className="items-center text-[#64748b] text-[10.5px] leading-3.5 tracking-[1.575px] uppercase font-bold">Get more sales calls</p>
+                                    <p className="items-center text-[#64748b] text-[10.5px] leading-3.5 tracking-[1.575px] uppercase font-bold">Clean, maintable code</p>
                                 </li>
 
                                 <li className="flex items-center gap-3">
                                     <div className=" pr-px flex w-4 h-4 justify-center items-center rounded-full bg-white/3 border border-white/10">
                                         <Check color="#475569" size={12} strokeWidth={3} />
                                     </div>
-                                    <p className="items-center text-[#64748b] text-[10.5px] leading-3.5 tracking-[1.575px] uppercase font-bold">Sell more products</p>
+                                    <p className="items-center text-[#64748b] text-[10.5px] leading-3.5 tracking-[1.575px] uppercase font-bold">Deployment and hosting</p>
                                 </li>
 
                                 <li className="flex items-center gap-3">
                                     <div className=" pr-px flex w-4 h-4 justify-center items-center rounded-full bg-white/3 border border-white/10">
                                         <Check color="#475569" size={12} strokeWidth={3} />
                                     </div>
-                                    <p className="items-center text-[#64748b] text-[10.5px] leading-3.5 tracking-[1.575px] uppercase font-bold">Improve overall conversion rates</p>
+                                    <p className="items-center text-[#64748b] text-[10.5px] leading-3.5 tracking-[1.575px] uppercase font-bold">WordPress, Shopify & Custom</p>
                                 </li>
                             </ul>
                         </div>
