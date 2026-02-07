2b972dd5b77d53659494bab8d998abeeb4d84e0b
Hissam <hissamdev@gmail.com>
2026-02-07 19:13:34 +0600

    Created and added jwt logic in middleware.ts

--- utils/jwt.ts
+++ utils/jwt.ts
@@ -1,15 +1,23 @@
 import jwt from 'jsonwebtoken';
 import Cookie from 'cookie';
 
-export function parseAuthCookie(cookieHeader: string | undefined): string | null {
+export type JWTPayload = {
+    userId: string;
+    iat?: number;
+    exp?: number;
+
+}
+
+// Return authToken string, otherwise null if cookieHeader or authToken doesn't exist
+export function parseAuthCookie(cookieHeader: string | null): string | null {
     if (!cookieHeader) return null;
     const cookies = Cookie.parse(cookieHeader);
     return cookies.authToken || null;
 }
 
-export function verifyJwt(token: string) {
+export function verifyJwt(token: string): JWTPayload | null {
     try {
-        return jwt.verify(token, process.env.JWT_SECRET!)
+        return jwt.verify(token, process.env.JWT_SECRET!) as JWTPayload
     } catch (err) {
         console.error('JWT verification failed:', err);
         return null;
