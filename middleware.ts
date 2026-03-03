60d87116bb36171a710328ca6e6d2e82a8932770
Hissam <hissamdev@gmail.com>
2026-03-03 15:15:51 +0600

    Downgrade to prisma 6

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,5 @@
 import { NextResponse } from "next/server";
 import { NextRequest } from "next/server";
-import { parseAuthCookie, verifyJwt } from "./utils/jwt";
-
-export const runtime = 'experimental-edge'
 
 export async function middleware(request: NextRequest) {
     return NextResponse.next()
