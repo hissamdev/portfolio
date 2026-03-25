6c1ad1ea4f6ada9aaceaa8ce39d6bc77349e890b
Hissam <hissamdev@gmail.com>
2026-03-25 15:30:02 +0600

    APIs set for fetching blog content

--- app/api/blogs/[slug]/route.ts
+++ app/api/blogs/[slug]/route.ts
@@ -1,6 +1,13 @@
 import { db } from "@/lib/db";
+import { blogTable } from "@/lib/db/schema";
+import { eq } from "drizzle-orm";
 import { NextResponse } from "next/server";
 
 export async function GET(req: Request, { params }: { params: Promise<{ slug: string }> }) {
-    
+    const{ slug } = await params;
+
+    const post = await db.select().from(blogTable).where(eq(blogTable.slug, slug))
+    if (!post) return NextResponse.json({ message: "Post not found" }, { status: 404 })
+
+    return NextResponse.json(post);
 }
