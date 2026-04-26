903052c1ad9a6cc8a3c7916b6808f4b7ac3c3d6a
Hissam <hissamdev@gmail.com>
2026-04-26 09:04:08 +0600

    Changes to projects section

--- app/components/home-ui/Projects.tsx
+++ app/components/home-ui/Projects.tsx
@@ -1,113 +1,193 @@
-'use client'
+"use client";
 import { ArrowRight } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 import { useEffect, useState } from "react";
-import clsx from 'clsx';
+import clsx from "clsx";
 import { motion, AnimatePresence } from "framer-motion";
 import { getProjects } from "@/app/actions/projects";
 import { InferSelectModel } from "drizzle-orm";
 import { blogTable, projectTable } from "@/lib/db/schema";
 import { db } from "@/lib/db";
 
-type Projects = InferSelectModel<typeof projectTable>
+type Projects = InferSelectModel<typeof projectTable>;
 
 export default function Projects() {
     const [category, setCategory] = useState("all");
-    const [projects, setProjects] = useState<Projects[]>([])
 
-    useEffect(() => {
-        async function load() {
-            const res = await getProjects()
+    const projects = [
+        {
+            heading: "Some kinda heading",
+            description: "Some sort of description goes here",
+            imageAlt: "Some alt text",
+            tags: [{ name: "React" }, { name: "Something Else" }],
+        },
+        {
+            heading: "Some other kinda heading",
+            description: "Some sort of description goes here",
+            imageAlt: "Some alt text",
+            tags: [{ name: "React" }, { name: "Something Else" }],
+        },
+        {
+            heading: "Some kinda real heading",
+            description: "Some sort of description goes here",
+            imageAlt: "Some alt text",
+            tags: [{ name: "React" }, { name: "Something Else" }],
+        },
+        {
+            heading: "Some other fake kinda heading",
+            description: "Some sort of description goes here",
+            imageAlt: "Some alt text",
+            tags: [{ name: "React" }, { name: "Something Else" }],
+        },
+        {
+            heading: "Some other rare kinda heading",
+            description: "Some sort of description goes here",
+            imageAlt: "Some alt text",
+            tags: [{ name: "React" }, { name: "Something Else" }],
+        },
+    ];
 
-            setProjects(res ?? [])
-            console.log(res)
-        }
+    const projectsToDisplay =
+        category === "all"
+            ? projects
+            : projects.filter((project: any) =>
+                  project.tags.some((tag: any) => tag.name === category),
+              );
 
-        load();
-    }, [])
-    
-    const projectsToDisplay = category === "all" ? projects : projects.filter((project: any) => project.tags.some((tag: any) => tag.name === category))
-    
     return (
-        <section className="px-[21px] md:px-0 pt-28 max-w-6xl mx-auto">
+        <section className="px-5.25 md:px-0 pt-28 max-w-361.5 mx-auto">
             <div className="flex items-center gap-2.5">
-                <div className="w-[73px] h-px bg-[#5856E6]"></div>
-                <h2 className="md:text-[17.5px] md:leading-[21px] text-[#5856E6] font-bold">Skills</h2>
+                <div className="w-18.25 h-px bg-[#5856E6]"></div>
+                <h2 className="md:text-[17.5px] md:leading-5.25 text-[#5856E6] font-bold">
+                    Projects
+                </h2>
             </div>
             <div className="md:flex justify-between">
                 <div>
-                    <h2 className="md:pt-[9px] text-[23.5px] leading-7 font-bold">Previous work and projects</h2>
-                    <p className="md:pt-[17px] md:w-[592px] text-[15.75px] leading-[19px]"
-                    >
-                        Full Stack, Digital Marketing & SEO projects that I've previously worked on.
+                    <h2 className="md:pt-2.25 text-[23.5px] leading-7 font-bold">
+                        Previous work and projects
+                    </h2>
+                    <p className="md:pt-4.25 md:w-148 text-[15.75px] leading-4.75">
+                        Exploring the intersection of complex engineering,
+                        growth-focused SEO, and high-impact digital marketing.
                     </p>
                 </div>
-                
-                <div className="self-end">
-                    <div className="p-[5.25px] flex bg-[#EFEFEF] rounded-[10.5px]">
-                        <span 
-                            onClick={() => setCategory("all")}
-                            className={clsx("py-3 mr-[5px] px-[17px] text-[12.25px] leading-[15px] tracking-[1.75px] font-bold hover:text-gray-600 rounded-[7px] cursor-pointer",
-                            category === "all" && "bg-black text-white hover:text-white")}>
-                            All
-                        </span>
-                        <span 
-                            onClick={() => setCategory("FullStack")}
-                            className={clsx("py-3 mr-[5px] px-[17px] text-[12.25px] leading-[15px] tracking-[1.75px] font-bold hover:text-gray-600 rounded-[7px] cursor-pointer",
-                            category === "FullStack" && "bg-black text-white hover:text-white")}>
-                            Full Stack
-                        </span>
-                        <span 
-                            onClick={() => setCategory("SEO")}
-                            className={clsx("py-3 mr-[5px] px-[17px] text-[12.25px] leading-[15px] tracking-[1.75px] font-bold hover:text-gray-600 rounded-[7px] cursor-pointer",
-                            category === "SEO" && "bg-black text-white hover:text-white")}>
-                            SEO
-                        </span>
-                        <span 
-                            onClick={() => setCategory("dm")}
-                            className={clsx("py-3 mr-[5px] px-[17px] text-[12.25px] leading-[15px] tracking-[1.75px] font-bold hover:text-gray-600 rounded-[7px] cursor-pointer",
-                            category === "dm" && "bg-black text-white hover:text-white")}>
-                            Digital Marketing
-                        </span>
-                    </div>
-                </div>
-            </div>
 
-            <div className="pt-[62px] project-cards grid gap-y-8 md:grid-cols-[repeat(3,minmax(300,1fr))] md:gap-x-18 md:gap-y-12">
-                <AnimatePresence mode="popLayout">
-                    {projectsToDisplay.map((p: any, index: any) => {
-                        return (
-                            <motion.div
-                                key={p.id}
-                                layout
-                                whileHover={{
-                                    y: -10,
-                                    transition: { duration: 0.1, ease: 'easeOut' }
-                                }}
-                                className={clsx("py-2.5 px-[10.5px] h-96 rounded-[21px] shadow-lg")}>
-                                <div className="relative w-full h-[195px] rounded-[21px] cursor-pointer">
-                                    <Image
-                                        alt={p.imageAlt}
-                                        src="/Ahmed-Hissam-Recording-a-Resume.jpg"
-                                        fill
-                                        className="object-cover rounded-[21px]"
-                                    />
-                                </div>
-                                <div className="pt-6 px-[18px]">
-                                    <div>
-                                        {p.tags.map((tag: any) => <span key={tag.name} className="py-[3.5px] px-[10.5px] gap-2.5 text-[10px] leading-3 font-bold text-[#64748B] bg-[#64748B]/10 rounded-[5.25px]">{tag.name}</span> )}
-                                    </div>
-                                    <h3 className="pt-3.5 text-[17.5px] leading-[22px] font-bold">{p.heading}</h3>
-                                    <p className="pt-[10.5px] text-[12.25px] leading-[15px]">{p.description}</p>
-                                    <Link href={p.link ?? "/"} className="inline-flex items-center gap-1.5 mt-[21px] text-[12.25px] leading-[15px] font-bold">View Project <ArrowRight size={14} color="black" /></Link>
-                                </div>
-                            </motion.div>
-                        )
-                    })
-                    }
-                </AnimatePresence>
+                <CategorySelector
+                    category={category}
+                    setCategory={setCategory}
+                />
             </div>
+            <ProjectCards projectsToDisplay={projectsToDisplay} />
         </section>
-    )
-} 
+    );
+}
+
+export function ProjectCards({ projectsToDisplay }) {
+    return (
+        <div className="mt-15.5 project-cards grid gap-y-8 md:grid-cols-4 md:gap-x-18 md:gap-y-12">
+            <AnimatePresence mode="popLayout">
+                {projectsToDisplay.map((p: any, index: any) => {
+                    return (
+                        <motion.div
+                            key={p.heading}
+                            layout
+                            whileHover={{
+                                y: -10,
+                                transition: {
+                                    duration: 0.1,
+                                    ease: "easeOut",
+                                },
+                            }}
+                            className={clsx("min-h-96 shadow-lg")}
+                        >
+                            <div className="relative w-full h-48.75 cursor-pointer">
+                                <Image
+                                    alt={p.imageAlt}
+                                    src="/Ahmed-Hissam-Recording-a-Resume.jpg"
+                                    fill
+                                    className="object-cover"
+                                />
+                            </div>
+                            <div className="pt-6 px-4.5">
+                                <div>
+                                    {p.tags.map((tag: any) => (
+                                        <span
+                                            key={tag.name}
+                                            className="py-[3.5px] px-[10.5px] gap-2.5 text-[10px] leading-3 font-bold text-[#64748B] bg-[#64748B]/10 rounded-[5.25px]"
+                                        >
+                                            {tag.name}
+                                        </span>
+                                    ))}
+                                </div>
+                                <h3 className="pt-3.5 text-[17.5px] leading-5.5 font-bold">
+                                    {p.heading}
+                                </h3>
+                                <p className="pt-[10.5px] text-[12.25px] leading-3.75">
+                                    {p.description}
+                                </p>
+                                <Link
+                                    href={p.link ?? "/"}
+                                    className="inline-flex items-center gap-1.5 mt-5.25 text-[12.25px] leading-3.75 font-bold"
+                                >
+                                    View Project{" "}
+                                    <ArrowRight size={14} color="black" />
+                                </Link>
+                            </div>
+                        </motion.div>
+                    );
+                })}
+            </AnimatePresence>
+        </div>
+    );
+}
+
+export function CategorySelector({ category, setCategory }) {
+    return (
+        <div className="self-end">
+            <div className="p-[5.25px] flex bg-[#EFEFEF] rounded-[10.5px]">
+                <span
+                    onClick={() => setCategory("all")}
+                    className={clsx(
+                        "py-3 mr-1.25 px-4.25 text-[12.25px] leading-3.75 tracking-[1.75px] font-bold hover:text-gray-600 rounded-[7px] cursor-pointer",
+                        category === "all" &&
+                            "bg-black text-white hover:text-white",
+                    )}
+                >
+                    All
+                </span>
+                <span
+                    onClick={() => setCategory("FullStack")}
+                    className={clsx(
+                        "py-3 mr-[5px] px-[17px] text-[12.25px] leading-[15px] tracking-[1.75px] font-bold hover:text-gray-600 rounded-[7px] cursor-pointer",
+                        category === "FullStack" &&
+                            "bg-black text-white hover:text-white",
+                    )}
+                >
+                    Full Stack
+                </span>
+                <span
+                    onClick={() => setCategory("SEO")}
+                    className={clsx(
+                        "py-3 mr-[5px] px-[17px] text-[12.25px] leading-[15px] tracking-[1.75px] font-bold hover:text-gray-600 rounded-[7px] cursor-pointer",
+                        category === "SEO" &&
+                            "bg-black text-white hover:text-white",
+                    )}
+                >
+                    SEO
+                </span>
+                <span
+                    onClick={() => setCategory("dm")}
+                    className={clsx(
+                        "py-3 mr-[5px] px-[17px] text-[12.25px] leading-[15px] tracking-[1.75px] font-bold hover:text-gray-600 rounded-[7px] cursor-pointer",
+                        category === "dm" &&
+                            "bg-black text-white hover:text-white",
+                    )}
+                >
+                    Digital Marketing
+                </span>
+            </div>
+        </div>
+    );
+}
--- app/components/home-ui/hero-section-ui/FeaturedProjects.tsx
+++ app/components/home-ui/hero-section-ui/FeaturedProjects.tsx
@@ -1,11 +1,95 @@
+import Image from "next/image";
+
+type ZigZagProps = {
+    title: string;
+    text: string;
+    tags: string[];
+    reversed: boolean;
+    imageAlt: string;
+    imageUrl: string;
+};
+
+export function ZigZag({
+    title,
+    text,
+    tags,
+    reversed,
+    imageAlt,
+    imageUrl,
+}: ZigZagProps) {
+    return (
+        <div
+            className={`flex justify-between items-center md:gap-14 ${reversed ? "flex-row-reverse" : "flex-row"}`}
+        >
+            <div>
+                <div className="relative aspect-613/367 w-140">
+                    <Image
+                        alt={imageAlt}
+                        src={imageUrl}
+                        fill
+                        className="object-contain"
+                    />
+                </div>
+            </div>
+            <div>
+                <h3 className="text-3xl font-medium">{title}</h3>
+                <p className="max-w-177.25 mt-4 text-[20px] leading-8 ">
+                    {text}
+                </p>
+            </div>
+        </div>
+    );
+}
+
 export default function FeaturedProjects() {
     return (
-        <section>
-            <div className="max-w-8xl w-full mx-auto">
-                <span className="text-center uppercase text-lg ">
+        <section className="py-12">
+            <div className="max-w-320 w-full mx-auto">
+                <h3 className="text-center uppercase text-[#5856E6] text-lg font-urbanist">
                     Driving Impact
-                </span>
-                <h2>My Featured Work</h2>
+                </h3>
+                <h2 className="mt-3 text-center text-5xl font-medium">
+                    My Featured Work
+                </h2>
+                <p className="mt-4 text-center">
+                    High impact projects that influence sales
+                </p>
+
+                <div className="md:mt-20 flex flex-col md:gap-12">
+                    <ZigZag
+                        title="Title Goes Here and is Awesome"
+                        text="I'm Ahmed Hissam, a World-Class Senior Full-Stack
+                            Engineer based in New York City. My journey in tech
+                            started over 8 years ago, driven by a fascination with
+                            how code can build entirely new digital worlds."
+                        tags={["React", "TypeScript"]}
+                        reversed={false}
+                        imageAlt="SEO Website Audit Template"
+                        imageUrl="/assets/stylish-ecommerce-scores.png"
+                    />
+                    <ZigZag
+                        title="Title Goes Here and is Awesome"
+                        text="I'm Ahmed Hissam, a World-Class Senior Full-Stack
+                            Engineer based in New York City. My journey in tech
+                            started over 8 years ago, driven by a fascination with
+                            how code can build entirely new digital worlds."
+                        tags={["React", "TypeScript"]}
+                        reversed={true}
+                        imageAlt="SEO Website Audit Template"
+                        imageUrl="/assets/website-audit.png"
+                    />
+                    <ZigZag
+                        title="Title Goes Here and is Awesome"
+                        text="I'm Ahmed Hissam, a World-Class Senior Full-Stack
+                            Engineer based in New York City. My journey in tech
+                            started over 8 years ago, driven by a fascination with
+                            how code can build entirely new digital worlds."
+                        tags={["React", "TypeScript"]}
+                        reversed={false}
+                        imageAlt="SEO Website Audit Template"
+                        imageUrl="/assets/portfolio-about-old.png"
+                    />
+                </div>
             </div>
         </section>
     );
