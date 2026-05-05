4c995925f07a21ba2c758f99f71a0fc318198335
Hissam <hissamdev@gmail.com>
2026-05-05 13:50:32 +0600

    Created services section layout

--- app/components/home-ui/Services.tsx
+++ app/components/home-ui/Services.tsx
@@ -1,17 +1,81 @@
 export default function Services() {
     return (
         <section className="py-32">
-            <div className="py-10 border-y border-black text-7xl font-bold font-urbanist uppercase text-transparent [-webkit-text-stroke:1px_black] overflow-hidden whitespace-nowrap animate-marque">
-                <p className="inline-block">
-                    Full Stack Developer * React.js * Next.js * TypeScript *
-                    Projects Available *
-                </p>
-                <p className="inline-block">
-                    Full Stack Developer * React.js * Next.js * TypeScript *
-                    Projects Available *
-                </p>
+            <div className="py-10 border-y border-black text-7xl font-bold font-urbanist uppercase text-transparent [-webkit-text-stroke:1px_black] overflow-hidden whitespace-nowrap">
+                <div className="marque-scroll flex">
+                    <p className="px-4">
+                        Full Stack Developer * React.js * Next.js * TypeScript *
+                        Projects Available * Full Stack Developer * React.js *
+                        Next.js * TypeScript * Projects Available *
+                    </p>
+                    <p className="px-4">
+                        Full Stack Developer * React.js * Next.js * TypeScript *
+                        Projects Available * Full Stack Developer * React.js *
+                        Next.js * TypeScript * Projects Available *
+                    </p>
+                </div>
             </div>
-            <div className="max-w-322.75 w-full mx-auto">hi</div>
+            <div className="py-40 max-w-322.75 w-full mx-auto">
+                <h2 className="text-center">Services</h2>
+                <h3 className="text-center">My Expertise</h3>
+
+                <div className="pt-25">
+                    <div className="divide-y border">
+                        <div className="h-43.75 flex items-center justify-between px-12">
+                            <div className="flex gap-4 max-w-100 w-full text-[31.5px] font-bold">
+                                <span>01</span>
+                                <span>Web Development</span>
+                            </div>
+                            <p className="max-w-sm w-full mr-40 text-[20.75px]">
+                                I'm Ahmed Hissam, a World-Class Senior
+                                Full-Stack Engineer based in New York City.
+                            </p>
+                            <div className="">
+                                <div className="bg-black rounded-full w-8 aspect-square"></div>
+                            </div>
+                        </div>
+                        <div className="h-43.75 flex items-center justify-between px-12">
+                            <div className="flex gap-4 max-w-100 w-full text-[31.5px] font-bold">
+                                <span>02</span>
+                                <span>Expert SEO</span>
+                            </div>
+                            <p className="max-w-sm w-full mr-40 text-[20.75px]">
+                                I'm Ahmed Hissam, a World-Class Senior
+                                Full-Stack Engineer based in New York City.
+                            </p>
+                            <div className="">
+                                <div className="bg-black rounded-full w-8 aspect-square"></div>
+                            </div>
+                        </div>
+                        <div className="h-43.75 flex items-center justify-between px-12">
+                            <div className="flex gap-4 max-w-100 w-full text-[31.5px] font-bold">
+                                <span>03</span>
+                                <span>Digital Marketing</span>
+                            </div>
+                            <p className="max-w-sm w-full mr-40 text-[20.75px]">
+                                I'm Ahmed Hissam, a World-Class Senior
+                                Full-Stack Engineer based in New York City.
+                            </p>
+                            <div className="">
+                                <div className="bg-black rounded-full w-8 aspect-square"></div>
+                            </div>
+                        </div>
+                        <div className="h-43.75 flex items-center justify-between px-12">
+                            <div className="flex gap-4 max-w-100 w-full text-[31.5px] font-bold">
+                                <span>01</span>
+                                <span>Web Development</span>
+                            </div>
+                            <p className="max-w-sm w-full mr-40 text-[20.75px]">
+                                I'm Ahmed Hissam, a World-Class Senior
+                                Full-Stack Engineer based in New York City.
+                            </p>
+                            <div className="">
+                                <div className="bg-black rounded-full w-8 aspect-square"></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </section>
     );
 }
